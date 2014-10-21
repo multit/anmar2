116e90c5f6ad6191dfa2136f5bf3298ccbb56bb0
multit <andrea.fiorelli@gmail.com>
2014-10-21 14:27:57 +0200

    masonry aggiunto

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@ var app = (function(document, $) {
 
 	'use strict';
 	var docElem = document.documentElement,
-
+		
 		_userAgentInit = function() {
 			docElem.setAttribute('data-useragent', navigator.userAgent);
 		},
@@ -17,19 +17,45 @@ var app = (function(document, $) {
 
 })(document, jQuery);
 
+
+
 (function() {
 
 	'use strict';
 	app.init();
-
+	masonryControl();
+	
 })();
-
-
-//$('.banner-sostieni').css({ y: -200 });
-//$('.banner-sostieni').transition({ y: 0, delay:500, easing:"easeOutQuad" });
-
 
 
 $('#HPanmar-banner')
 	.css({ y: -300 })
 	.transition({ y: 0, easing:"easeOutQuad", delay:400 })
+
+// Masonry version */
+
+$(window).load(function(){   
+	var masnry_container = document.querySelector('#HParticoli');
+	var msnry = new Masonry( masnry_container, {
+		
+  		gutter:18,
+  		itemSelector: '.articolo'
+	});
+});
+
+$( window ).resize(function() {
+	console.log('screen size: ' + $(window).width());
+	masonryControl();
+});	
+
+function masonryControl() {
+	var fi  = $(window).width();
+	(fi<550) ? $('.articolo').css('width','100%'):
+	(fi>=550 && fi<=900) ? $('.articolo').css('width','45%'):
+	(fi>900) ? $('.articolo').css('width','32%'):
+	"dimensione non prevista";
+}
+
+
+
+
