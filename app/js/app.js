9ec1137636ab757f7c45e6e830af8b1b9c43d244
multit <andrea.fiorelli@gmail.com>
2014-11-12 10:30:18 +0100

    Primo layout con Foundation

--- app/js/app.js
+++ app/js/app.js
@@ -30,14 +30,13 @@ var app = (function(document, $) {
 
 $('#HPanmar-banner')
 	.css({ y: -300 })
-	.transition({ y: 0, easing:"easeOutQuad", delay:400 })
+	.transition({ y: 0, easing:'easeOutQuad', delay:400 });
 
 // Masonry version */
 
 $(window).load(function(){   
 	var masnry_container = document.querySelector('#HParticoli');
-	var msnry = new Masonry( masnry_container, {
-		
+	var msnry = new Masonry( masnry_container, {		
   		gutter:18,
   		itemSelector: '.articolo'
 	});
@@ -53,7 +52,7 @@ function masonryControl() {
 	(fi<550) ? $('.articolo').css('width','100%'):
 	(fi>=550 && fi<=900) ? $('.articolo').css('width','45%'):
 	(fi>900) ? $('.articolo').css('width','32%'):
-	"dimensione non prevista";
+	'dimensione non prevista';
 }
 
 
